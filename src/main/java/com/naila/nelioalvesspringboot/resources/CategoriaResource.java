32bee99e09968019902f62461d27cf44986d5171
Naila <nailaoli@gmail.com>
2021-01-15 18:00:27 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/naila/nelioalvesspringboot/resources/CategoriaResource.java
+++ src/main/java/com/naila/nelioalvesspringboot/resources/CategoriaResource.java
@@ -1,15 +1,21 @@
 package com.naila.nelioalvesspringboot.resources;
 
+import com.naila.nelioalvesspringboot.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String listar(){
-        return "REST funciona!";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        return Arrays.asList(cat1, cat2);
     }
 }
