edd19c7789628f7baf1589bb87c32c531280beca
Naila <nailaoli@gmail.com>
2021-01-15 00:04:18 -0300

    Projeto Spring Boot criado

--- src/main/java/com/naila/nelioalvesspringboot/NelioalvesspringbootApplication.java
+++ src/main/java/com/naila/nelioalvesspringboot/NelioalvesspringbootApplication.java
@@ -1,13 +1,13 @@
-package com.naila.springboot.aplicacao;
+package com.naila.nelioalvesspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class NelioalvesspringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication.run(NelioalvesspringbootApplication.class, args);
     }
 
 }
--- src/test/java/com/naila/nelioalvesspringboot/NelioalvesspringbootApplicationTests.java
+++ src/test/java/com/naila/nelioalvesspringboot/NelioalvesspringbootApplicationTests.java
@@ -1,10 +1,10 @@
-package com.naila.springboot;
+package com.naila.nelioalvesspringboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootApplicationTests {
+class NelioalvesspringbootApplicationTests {
 
     @Test
     void contextLoads() {
