db86a5caab05a3b7ad448a5bda94c898098b6775
Naila <nailaoli@gmail.com>
2021-01-16 11:41:31 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/naila/nelioalvesspringboot/NelioalvesspringbootApplication.java
+++ src/main/java/com/naila/nelioalvesspringboot/NelioalvesspringbootApplication.java
@@ -1,7 +1,9 @@
 package com.naila.nelioalvesspringboot;
 
 import com.naila.nelioalvesspringboot.domain.Categoria;
+import com.naila.nelioalvesspringboot.domain.Produto;
 import com.naila.nelioalvesspringboot.repositories.CategoriaRepository;
+import com.naila.nelioalvesspringboot.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +16,8 @@ public class NelioalvesspringbootApplication implements CommandLineRunner {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(NelioalvesspringbootApplication.class, args);
@@ -23,6 +27,15 @@ public class NelioalvesspringbootApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
+        Produto prod1 = new Produto(null, "Computador", 2000.00);
+        Produto prod2 = new Produto(null, "Impressora", 800.00);
+        Produto prod3 = new Produto(null, "Mouse", 80.00);
+        cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+        cat2.getProdutos().add(prod2);
+        prod1.getCategorias().add(cat1);
+        prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        prod3.getCategorias().add(cat1);
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
     }
 }
---
+++ src/main/java/com/naila/nelioalvesspringboot/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.naila.nelioalvesspringboot.repositories;
+
+import com.naila.nelioalvesspringboot.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
